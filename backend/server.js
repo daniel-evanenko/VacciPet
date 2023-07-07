852dec720aaa968940a22e0961ea8bf2afb82b54
daniel <daniel.evanenko@gmail.com>
2023-07-08 01:34:29 +0300

    fix cors problem and register user

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,25 @@ const cookieParser = require('cookie-parser')
 const routes = require('./routes/routes')
 const URI = "mongodb://localhost:27017/vaccipet"
 const PORT = 3000;
+var whitelist = ['http://localhost:4200', 'http://localhost:8080']; //white list consumers
+
 const app = express()
-
-app.use(cors({
-    credentials:true,
-    origin:['*']
-}))
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(null, false);
+      }
+    },
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+  };
+  
+  app.use(cors(corsOptions));
 app.use(cookieParser())
-
 app.use(express.json())
 
 app.use("/api",routes)
