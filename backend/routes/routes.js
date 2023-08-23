391cca335aa049c8200f5324d2d9fad1ae8b5150
daniel <daniel.evanenko@gmail.com>
2023-08-23 22:00:53 +0300

    add backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-
 const User = require('../models/user')
-
 router.post('/register',async (req,res) => {
 
     const salt = await bcrypt.genSalt(10);
